49dc93b132c9328bbb5b158e5d91c413fd63d966
janhenrik.haukeland <janhenrik.haukeland@8a3d18e2-db48-0410-abc8-4378e866053e>
2009-07-16 01:59:43 +0000

    Sublicense MIT code

--- src/exceptions/Exception.c
+++ src/exceptions/Exception.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2004-2009 Tildeslash Ltd. All rights reserved.
+ * Copyright (c) 1994,1995,1996,1997 by David R. Hanson.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3.
@@ -12,31 +13,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-/*
- * Copyright (c) 1994,1995,1996,1997 by David R. Hanson.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"), 
- * to deal in the Software without restriction, including without limitation 
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- * and/or sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.
- * 
- * <http://www.opensource.org/licenses/mit-license.php>
- */
-
 
 #include "Config.h"
 
@@ -51,10 +27,9 @@
  * Implementation of the Exception interface. Defines the Thread local 
  * Exception stack and Exceptions used in the library. 
  * 
- * LEGAL NOTICE, the part that differs from the original code is Copyright
- * (C) Tildeslash Ltd. This implementation is licensed under GPLv3. The MIT
- * license above applies to the original and exceptional clever CII code which
- * can be found at this URL http://www.cs.princeton.edu/software/cii/
+ * This implementation is a minor modification of the Except code found 
+ * in David R. Hanson's excellent book "C Interfaces and Implementations".
+ * See http://www.cs.princeton.edu/software/cii/
  *
  * @file
  */
