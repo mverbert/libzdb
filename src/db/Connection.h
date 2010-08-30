969ccd7a9dd3e9fbe852d6627ed5753542167aeb
janhenrik.haukeland <janhenrik.haukeland@8a3d18e2-db48-0410-abc8-4378e866053e>
2010-08-30 18:26:21 +0000

    Unclear ResultSet on Connection_prepareStatement, instead use Connection_clear if statement sequence error

--- src/db/Connection.h
+++ src/db/Connection.h
@@ -40,9 +40,9 @@
  * If an error occur during execution, the method Connection_getLastError()
  * can be used to obtain a string describing the error. The method 
  * Connection_executeQuery() will return an empty ResultSet (not null) if
- * the SQL statement did not return any values.  A ResultSet lives until
- * next Connection execute, next Connection prepare statement or until the
- * Connection is returned to the Connection Pool.
+ * the SQL statement did not return any values.  A ResultSet lives until the
+ * next call to Connection execute or until the Connection is returned
+ * to the Connection Pool.
  *
  * Any SQL statement that changes the database (basically, any SQL
  * command other than SELECT) will automatically start a transaction
