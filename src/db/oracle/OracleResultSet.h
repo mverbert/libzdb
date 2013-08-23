1dbfed237dc806a3b0d470e085e3b382ae6749c0
tvn <tvntsr@yahoo.com>
2013-08-23 23:37:59 +0300

    License improvements

--- src/db/oracle/OracleResultSet.h
+++ src/db/oracle/OracleResultSet.h
@@ -1,29 +1,28 @@
 /*
- * Copyright (C) 2010 Volodymyr Tarasenko <tvntsr@yahoo.com>
- *               2010 Sergey Pavlov <sergey.pavlov@gmail.com>
- *               2010 PortaOne Inc.
+ * Copyright (C) 2010-2013 Volodymyr Tarasenko <tvntsr@yahoo.com>
+ *              2010      Sergey Pavlov <sergey.pavlov@gmail.com>
+ *              2010      PortaOne Inc.
  * Copyright (C) Tildeslash Ltd. All rights reserved.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 3.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * In addition, as a special exception, the copyright holders give
- * permission to link the code of portions of this program with the
- * OpenSSL library under certain conditions as described in each
- * individual source file, and distribute linked combinations
- * including the two.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You must obey the GNU General Public License in all respects
- * for all of the code used other than OpenSSL.
- */
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */ 
+
 #ifndef ORACLE_RESULTSET_INCLUDED
 #define ORACLE_RESULTSET_INCLUDED
 #define T ResultSetDelegate_T
