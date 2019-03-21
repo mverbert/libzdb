63ed8b9d32dbeaad5a890ecf9a2812bbc06f9c99
Tildeslash <hauk@tildeslash.com>
2019-03-21 02:24:15 +0100

    Remove comment about query timeout as it no longer apply.

--- src/db/mysql/MysqlConnection.c
+++ src/db/mysql/MysqlConnection.c
@@ -38,16 +38,6 @@
 /**
  * Implementation of the Connection/Delegate interface for mysql.
  *
- * TODO: Query Timeout has no effect as this is not implemented in MySQL.
- * MySQL does not provide a general way to do this. The MySQL
- * JDBC driver implements query timeout by running a separate timer
- * thread and then KILL the process in the server if query execution
- * time exceed timeout. This is a brute force approach with potential
- * resource side-effects which we'd rather not use.
- *
- * If you use innodb, setting innodb_lock_wait_timeout in the server
- * can be a possible alternative. Default innodb_lock_wait_timeout is 50 sec.
- *
  * @file
  */
 
